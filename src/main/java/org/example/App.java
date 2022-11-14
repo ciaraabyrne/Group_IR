6787134b97eaf0b2e318d5a823291a8e003d0e90
Ciara Byrne <byrnec66@tcd.ie>
2022-11-14 17:36:26 +0000

    La Times Parsed by Ciara. Added pom.xml and Corpuses

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.*;
+import java.io.File;
+import java.util.ArrayList;
+
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+
+        LATimes.indexInput(args);
     }
 }
